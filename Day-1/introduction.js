92f6b1635b0803b1ce37e0785ca8b0781a0ecb70
Uygar Süslü <uygarsuslu1@gmail.com>
2022-03-10 21:10:45 +0300

    introduction added

---
+++ Day-1/introduction.js
@@ -0,0 +1,7 @@
+console.log("bu yazı introduction.js'den geldi");
+
+console.log(typeof "uygar");
+console.log(typeof 5);
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof undefined);
