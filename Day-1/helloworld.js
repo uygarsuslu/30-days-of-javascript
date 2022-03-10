b76719e722557319b64d1b0f4738ceca6b4ea123
Uygar Süslü <uygarsuslu1@gmail.com>
2022-03-10 21:10:31 +0300

    hello world added

---
+++ Day-1/helloworld.js
@@ -0,0 +1,13 @@
+console.log("hello world");
+
+let youtubeChannel = "prototurk",
+    channelAge = 11,
+    isChannelStillActive = true,
+    secondChannel = null,
+    channelMoney = undefined
+
+const PI = 3.14
+const GRAVITY = 9.81
+
+console.log(youtubeChannel, channelAge, isChannelStillActive, secondChannel, channelMoney);
+console.log(PI, GRAVITY);
