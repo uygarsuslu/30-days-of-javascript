3013d24014258c0d745a05afdb67d79ff5809c98
Uygar Süslü <uygarsuslu1@gmail.com>
2022-03-11 13:57:20 +0300

    changing data type added

---
+++ Day-2/changing-data-type.js
@@ -0,0 +1,12 @@
+// ********** CHANGING DATA TYPE ********** //
+
+// *** String to Int *** //
+let num1 = '10'
+let numInt = parseInt(num1)
+console.log("parseInt:", numInt) // 10
+
+// *** String to Float *** //
+let num2 = '9.81'
+let numFloat = parseFloat(num2)
+console.log(numFloat) // 9.81
+/* *************************************************************************************** */
