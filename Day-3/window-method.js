613c21c9d7736ef9a38a0f7c1f33c4f66f7ffb83
Uygar Süslü <uygarsuslu1@gmail.com>
2022-03-12 20:29:49 +0300

    window method added

---
+++ Day-3/window-method.js
@@ -0,0 +1,16 @@
+// ****** Window alert Method ****** //
+
+alert('Welcome to 30 Days Of JavaScript')
+/* *************************************************************************************** */
+
+// ****** Window prompt Method ****** //
+
+let number = prompt('Bir sayı giriniz:', 'sadece pozitif sayı girmelisiniz')
+console.log(number)
+/* *************************************************************************************** */
+
+// ****** Window confirm Method ****** //
+
+const isDelete = confirm('Silmek istediğine emin misin?')
+console.log(isDelete ? "Silme işlemi başarılı" : "Silme işlemi iptal edildi")
+/* *************************************************************************************** */
