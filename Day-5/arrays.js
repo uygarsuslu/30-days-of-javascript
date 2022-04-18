6dba9ee81728fc845865d7a7bce347f02a545f66
Uygar Süslü <uygarsuslu1@gmail.com>
2022-04-18 12:08:54 +0300

    day 5 added

--- Day-5/arrays.js
+++ Day-5/arrays.js
@@ -1,4 +1,5 @@
 // ****** split kullanarak dizi oluşturma ****** //
+
 let js = 'JavaScript'
 const charsInJavaScript = js.split('')
 console.log("split:", charsInJavaScript);
@@ -7,70 +8,89 @@ let companiesString = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
 const companies = companiesString.split(',')
 console.log("split:", companies);
 /* *************************************************************************************** */
+console.log("////////////////////////////////////////////////////////////////////////////")
 
 // ****** dizin öğelerine index kullanarak erişme ****** //
+
 const fruits = ['banana', 'orange', 'mango', 'lemon']
 console.log("length:", fruits.length) // 4
 
 let lastIndex = fruits.length - 1
 console.log("last index:", fruits[lastIndex]) // lemon
 /* *************************************************************************************** */
+console.log("////////////////////////////////////////////////////////////////////////////")
 
 // ****** fill: statik diziler oluşturma ****** //
+
 const eightXvalues = Array(3).fill('X') // it creates eight element values filled with 'X'
 console.log("fill:", eightXvalues)
 /* *************************************************************************************** */
+console.log("////////////////////////////////////////////////////////////////////////////")
 
 // ****** concat: dizi birleştirme ****** //
+
 const firstList = [1, 2, 3]
 const secondList = [4, 5, 6]
 console.log("concat:", firstList.concat(secondList, 7, 8, 9))
 /* *************************************************************************************** */
+console.log("////////////////////////////////////////////////////////////////////////////")
 
 // ****** indexOf: aranan elemanın dizide bulunup bulunmadığını kontrol eder ****** //
+
 const numbers = [1, 2, 3, 4, 5]
 // varsa index değerini yoksa -1 döner // ilk bulduğu değeri döner 
 console.log("indexOf:", numbers.indexOf(5)) // 4
 console.log("indexOf:", numbers.indexOf(0)) // -1
 /* *************************************************************************************** */
+console.log("////////////////////////////////////////////////////////////////////////////")
 
 // ****** lastIndexOf: aranan elemanın dizide bulunup bulunmadığını kontrol eder  ****** //
+
 const numbers1 = [1, 2, 3, 4, 5, 3, 1, 2]
 // varsa index değerini yoksa -1 döner // son bulduğu değeri döner
 console.log("lastIndexOf:", numbers1.lastIndexOf(2)) // 7
 console.log("lastIndexOf:", numbers1.lastIndexOf(0)) // -1 
 /* *************************************************************************************** */
+console.log("////////////////////////////////////////////////////////////////////////////")
 
 // ****** includes: dizide bir öğenin olup olmadığını kontrol eder ****** //
+
 const numbers2 = [1, 2, 3, 4, 5]
 // varsa true, yoksa false değerini döndürür  // büyük - küçük harf duyarlı 
 console.log("includes:", numbers2.includes(5)) // true
 console.log("includes:", numbers2.includes(0)) // false
 /* *************************************************************************************** */
+console.log("////////////////////////////////////////////////////////////////////////////")
 
 // ****** Array.isArray: verinin array olup olmadığını kontrol eder ****** //
+
 const numbers3 = [1, 2, 3, 4, 5]
 console.log("Array.isArray:", Array.isArray(numbers3)) // true
 // array ise true, değilse false döner
 const number = 100
 console.log("Array.isArray:", Array.isArray(number)) // false
 /* *************************************************************************************** */
+console.log("////////////////////////////////////////////////////////////////////////////")
 
 // ****** toString: diziyi stringe çevirir ****** //
+
 const numbers4 = [1, 2, 3, 4, 5]
 console.log("toString:", numbers4.toString()) // 1,2,3,4,5
 /* *************************************************************************************** */
+console.log("////////////////////////////////////////////////////////////////////////////")
 
 // ****** join: dizideki her bir elemanı join'de verdiğimiz karaktere göre birleştirip string döner ****** //
-const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
 
+const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
 console.log("join():", names.join()) // Asabeneh,Mathias,Elias,Brook
 console.log("join(''):", names.join('')) //AsabenehMathiasEliasBrook
 console.log("join(' '):", names.join(' ')) //Asabeneh Mathias Elias Brook
 console.log("join(' # '):", names.join(' # ')) //Asabeneh # Mathias # Elias # Brook
 /* *************************************************************************************** */
+console.log("////////////////////////////////////////////////////////////////////////////")
 
 // ****** slice: diziyi istediğimiz şekilde parçalamaya yarar ****** //
+
 const names1 = ["Tayfun", "Uygar", "Gökalp", "Mehmet"]
 // son eleman dahil değil //
 console.log("slice():", names1.slice()) // tüm diziyi kopyalar
@@ -78,56 +98,74 @@ console.log("slice(0):", names1.slice(0)) // tüm diziyi kopyalar
 console.log("slice(0, names1.length):", names1.slice(0, names1.length)) // tüm diziyi kopyalar
 console.log("slice(1, 3):", names1.slice(1, 3)) // 
 /* *************************************************************************************** */
+console.log("////////////////////////////////////////////////////////////////////////////")
 
 // ****** splice: diziyi istediğimiz yerden bölmeye ve yeni eleman eklemeye yarar ****** //
+
 const names2 = ["Tayfun", "Uygar", "Gökalp", "Mehmet"]
 // ilk değer = index sırası // ikinci değer = kaç tane // üçüncü değer = yeni eklenecek eleman 
 console.log("splice(1, 2, 'Ömer', 'Ali'):", names2.splice(1, 2, "Ömer", "Ali")) // ["uygar", "gökalp"]
 console.log("splice:", names2) // ["Tayfun", "Ömer", "Ali", "Mehmet"]
 /* *************************************************************************************** */
+console.log("////////////////////////////////////////////////////////////////////////////")
 
 // ****** push: dizinin sonuna eleman ekler ****** //
+
 const arr = ['item1', 'item2', 'item3']
 arr.push('new item')
 console.log("push:", arr)
 /* *************************************************************************************** */
+console.log("////////////////////////////////////////////////////////////////////////////")
 
 // ****** pop: dizinin sonundaki elemanı siler ****** //
+
 const numbers5 = [1, 2, 3, 4, 5]
 console.log("çıkan pop:", numbers5.pop()) // 5
 console.log("pop:", numbers5) // [1, 2, 3, 4]
 /* *************************************************************************************** */
+console.log("////////////////////////////////////////////////////////////////////////////")
 
 // ****** shift: dizinin başındaki elemanı siler ****** //
+
 const numbers6 = [1, 2, 3, 4, 5]
 console.log("silinen shift:", numbers6.shift()) // 1
 console.log("shift:", numbers6) // [2, 3, 4, 5]
 /* *************************************************************************************** */
+console.log("////////////////////////////////////////////////////////////////////////////")
 
 // ****** unshift: dizinin başına eleman ekler ****** //
+
 const numbers7 = [1, 2, 3, 4, 5]
 numbers7.unshift(0)
 console.log("unshift:", numbers7) // [0,1,2,3,4,5]
 /* *************************************************************************************** */
+console.log("////////////////////////////////////////////////////////////////////////////")
 
 // ****** reverse: diziyi ters çevirir ****** //
+
 const numbers8 = [1, 2, 3, 4, 5]
 console.log("reverse:", numbers.reverse()) // 
 /* *************************************************************************************** */
+console.log("////////////////////////////////////////////////////////////////////////////")
 
 // ****** sort: diziyi alfabetik sıraya göre sıralar ****** //
+
 const word = ['React', 'CSS', 'HTML']
 console.log("sort:", word.sort()) // ["CSS", "HTML", "React"]
 /* *************************************************************************************** */
+console.log("////////////////////////////////////////////////////////////////////////////")
 
 // ****** array of array ****** //
+
 const frontEnd = ['HTML', 'CSS', 'JS']
 const backEnd = ['Node', 'MongoDB']
 const fullStack = [frontEnd, backEnd]
 console.log("array of array:", fullStack)   // [["HTML", "CSS", "JS"], ["Node", "MongoDB"]]
 /* *************************************************************************************** */
+console.log("////////////////////////////////////////////////////////////////////////////")
 
 // ****** split - reverse -join ****** //
+
 let myName = "uygar"
 // console.log(myName.split("").reverse().join(""))
 let changeMyName = myName.split("").reverse().join("")
--- Day-5/exercises.js
+++ Day-5/exercises.js
@@ -1,4 +1,5 @@
 // ****** get the first item, the middle item and the last item of the array ****** //
+
 const countries = ['Albania', 'Bolivia', 'Canada', 'Denmark', 'Ethiopia', 'Finland', 'Germany', 'Hungary', 'Ireland', 'Japan', 'Kenya']
 let lastIndex = (countries.length - 1)
 let middleIndex = (countries.length - 1) / 2
@@ -6,15 +7,19 @@ console.log("first:", countries[0])
 console.log("middle:", countries[middleIndex])
 console.log("last:", countries[lastIndex])
 /* *************************************************************************************** */
+console.log("////////////////////////////////////////////////////////////////////////////")
 
 // ****** print the array like as a sentence: Facebook, Google, Microsoft, Apple, IBM,Oracle and Amazon are big IT companies. ****** //
+
 const technology = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"]
 technology.pop()
 let item = technology.join(", ")
 console.log("sentence:", `${item} and Amazon are big IT companies.`)
 /* *************************************************************************************** */
+console.log("////////////////////////////////////////////////////////////////////////////")
 
 // ****** in the following shopping cart add, remove, edit items ****** //
+
 const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey']
 
 // add 'Meat' in the beginning of your shopping cart if it has not been already added
@@ -35,8 +40,9 @@ console.log("includes:", shoppingCart);
 shoppingCart.splice(3, 1, "Green Tea")
 console.log("splice:", shoppingCart);
 /* *************************************************************************************** */
+console.log("////////////////////////////////////////////////////////////////////////////")
 
-// ******  ****** //
+// ****** the following is an array of 10 students ages: ****** //
 const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
 
 // sort the array and find the min and max age
