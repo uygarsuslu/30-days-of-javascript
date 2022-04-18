a4c27cfeb7a32a3c906d86e5eff661825a890ba8
Uygar Süslü <uygarsuslu1@gmail.com>
2022-04-18 12:08:25 +0300

    day 2 added

--- Day-2/number-data-types.js
+++ Day-2/number-data-types.js
@@ -38,17 +38,25 @@ let nums3 = [4, 5, 6]
 let nums4 = nums3
 console.log("nums3 = nums4: ", nums3 == nums4);
 /* *************************************************************************************** */
+console.log("///////////////////////////////////////////////////////////////////////////")
+
 console.log("pi değeri: ", Math.PI)
 console.log("round pi değeri: ", Math.round(Math.PI)) // 0.5 altındaysa aşağı, üstündeyse yukarı yuvarlar
 console.log("ceil pi değeri: ", Math.ceil(Math.PI)) // tavan
 console.log("floor pi değeri: ", Math.floor(Math.PI)) // taban
 /* *************************************************************************************** */
+console.log("///////////////////////////////////////////////////////////////////////////")
+
 console.log("en düşük değer: ", Math.min(-5, 4, 2, 6, -8));
 console.log("en yüksek değer: ", Math.max(-5, 4, 2, 6, -8));
 /* *************************************************************************************** */
+console.log("///////////////////////////////////////////////////////////////////////////")
+
 console.log("random sayı 0-1: ", Math.random()); // 0 ile  0.9999 arasında
 console.log("random sayı 0-10: ", Math.round(Math.random() * 11)); // 0 ile  10 arasında
 /* *************************************************************************************** */
+console.log("///////////////////////////////////////////////////////////////////////////")
+
 console.log("-15'in mutlak değeri: ", Math.abs(-15));
 console.log("100'ün karekökü: ", Math.sqrt(100));
 console.log("3'ün karesi: ", Math.pow(3, 2));
--- Day-2/string-methods.js
+++ Day-2/string-methods.js
@@ -6,44 +6,80 @@ let lastIndex = ts.length - 1
 console.log("TypeScript length - 1: ", lastIndex) // 9
 console.log("TypeScript last ındex: ", ts[lastIndex]) // t
 /* *************************************************************************************** */
+console.log("///////////////////////////////////////////////////////////////////////////")
+
+// ****** toUpperCase() - toLowerCase() ****** //
+
 let word1 = "prototurk"
 console.log("toUpperCase: ", word1.toUpperCase()) // büyük harfe çevirir
 /* *************************************************************************************** */
 let word2 = "PROTOTÜRK"
 console.log("toLowerCase: ", word2.toLowerCase()) // küçük harfe çevirir
 /* *************************************************************************************** */
+console.log("///////////////////////////////////////////////////////////////////////////")
+
+// ****** substring() - substr() ****** //
+
 let country1 = 'Finland'
 console.log("substring: ", country1.substring(0, 3)) // ilk verilen değerden başlayıp ikinci verilen değere kadar alır
 /* *************************************************************************************** */
 let country2 = 'Finland'
 console.log("substr: ", country2.substr(3, 4)) // ilk verilen değerden başlayıp ikinci verilen değer kadar alır 
 /* *************************************************************************************** */
+console.log("///////////////////////////////////////////////////////////////////////////")
+
+// ****** split() ****** //
+
 let string1 = '30 Days Of JavaScript'
 console.log("split:", string1.split()) // -> ["30 Days Of JavaScript"]
 console.log("split:", string1.split(' '))  // -> ["30", "Days", "Of", "JavaScript"]
 console.log("split:", string1.split(''))  // -> ["3","0","","D","a","y","s","","O","f","","J","a","v","a","S","c","r","i","p","t"]
 /* *************************************************************************************** */
+console.log("///////////////////////////////////////////////////////////////////////////")
+
+// ****** includes() ****** //
+
 let country3 = 'Denmark'
 console.log("includes den:", country3.includes('den')) // false
 console.log("includes Den:", country3.includes('Den')) // true
 /* *************************************************************************************** */
+console.log("///////////////////////////////////////////////////////////////////////////")
+
+// ****** replace() ****** //
+
 let country4 = "Rusya"
 console.log("replace Rusya:", country4.replace("Rusya", "Türkiye"));
 /* *************************************************************************************** */
+console.log("///////////////////////////////////////////////////////////////////////////")
+
+// ****** indexOf() ****** //
+
 let string2 = '30 Days Of JavaScript'
 console.log("indexOf Days:", string2.indexOf('Days')) // 3
 console.log("indexOf days:", string2.indexOf('days')) // -1
 console.log("indexOf a:", string2.indexOf('a')) // 4
 console.log("indexOf JavaScript:", string2.indexOf('JavaScript')) // 11
 /* *************************************************************************************** */
+console.log("///////////////////////////////////////////////////////////////////////////")
+
+// ****** startsWith() ****** //
+
 let string3 = 'Love is the best to in this world'
 console.log("startsWith Love:", string3.startsWith('Love'))   // true
 console.log("startsWith love:", string3.startsWith('love'))   // false
 /* *************************************************************************************** */
+console.log("///////////////////////////////////////////////////////////////////////////")
+
+// ****** endsWith() ****** //
+
 let string4 = 'Love is the best to in this world'
 console.log("endsWith world:", string4.endsWith('world')) // true
 console.log("endsWith World:", string4.endsWith('World')) // false
 /* *************************************************************************************** */
+console.log("///////////////////////////////////////////////////////////////////////////")
+
+// ****** search() ****** //
+
 let string5 = 'I love JavaScript. If you do not love JavaScript what else can you love.'
 console.log("search love:", string5.search('love')) // 2
 /* *************************************************************************************** */
